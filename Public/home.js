6034ddfeb50f07fbe9f3a43d7c841a8201d6e097
Sudheer121 <sudheertripathithebest@gmail.com>
2020-10-10 03:58:32 +0530

    document.location added for mobiles

--- Public/home.js
+++ Public/home.js
@@ -15,7 +15,12 @@ function signOut()
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
     console.log('User signed out of google.');
-    window.location.replace(SITE_NAME);
+    
+    if (screen.width <= 699) {
+        document.location = SITE_NAME;
+    } else {
+        window.location.replace(SITE_NAME);
+    }
     });
 }
 
@@ -49,7 +54,11 @@ window.fbAsyncInit = function() {
 function fbLogout() {
     FB.logout(function(response) {
         console.log("Logged out of facebook")
-        window.location.replace(SITE_NAME);
+        if (screen.width <= 699) {
+            document.location = SITE_NAME;
+        } else {
+            window.location.replace(SITE_NAME);
+        }
     });
 }
 
